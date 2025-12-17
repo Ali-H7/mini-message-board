769e92beb10029433925fe5580b9c437e741ba81
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-12-17 08:48:18 +0300

    Allow users to submit their message

--- app.js
+++ app.js
@@ -17,12 +17,21 @@ const messages = [
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index', { messages: messages });
 });
 
-app.get('/new', (req, res) => res.send('New'));
+app.get('/new', (req, res) => {
+  res.render('form');
+});
+
+app.post('/new', (req, res) => {
+  const { userMessage, userName } = req.body;
+  messages.push({ text: userMessage, user: userName, added: new Date() });
+  res.redirect('/');
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
