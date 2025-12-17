ff26555ab029f6808b95c41990bfdd2de766cf93
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-12-17 07:26:32 +0300

    Create routes for the app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => res.send('Home'));
+app.get('/new', (req, res) => res.send('New'));
+
 const PORT = 3000;
 app.listen(PORT, (error) => {
   if (error) {
